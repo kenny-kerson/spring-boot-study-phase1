3ce0b4f21a7fdfb219e6c11295b7df61823075c7
KO DONGKYOON <kenny.kerson@gmail.com>
2018-10-27 23:50:33 +0900

    최초커밋

---
+++ src/main/java/com/kenny/kabang/springbootstudyphase1/SpringBootStudyPhase1Application.java
@@ -0,0 +1,12 @@
+package com.kenny.kabang.springbootstudyphase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyPhase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudyPhase1Application.class, args);
+	}
+}
---
+++ src/test/java/com/kenny/kabang/springbootstudyphase1/SpringBootStudyPhase1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kenny.kabang.springbootstudyphase1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootStudyPhase1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
