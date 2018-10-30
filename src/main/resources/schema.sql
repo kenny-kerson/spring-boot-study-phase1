9ac5ec96fd27eb4a7d6da27269d5454ef0d40ac5
kenny.k <kenny.k@kakaobank.com>
2018-10-30 19:21:10 +0900

    final_sample 중간변경사항 반영3

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,2 @@
-create table users (id varchar(255) not null, age varchar(255), name varchar(255), primary key (id));
+drop table if exists users cascade
+create table users (id varchar(255) not null, age varchar(255), name varchar(255), primary key (id))
--- src/test/java/com/kenny/kabang/springbootstudyphase1/repository/UsersRepositoryTest.java
+++ src/test/java/com/kenny/kabang/springbootstudyphase1/repository/UsersRepositoryTest.java
@@ -4,17 +4,15 @@ import com.kenny.kabang.springbootstudyphase1.model.Users;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 public class UsersRepositoryTest {
 
     @Autowired
